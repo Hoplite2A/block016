afcc6123030ac4410b59360a630c06d009cbc623
Hoplite2A <stgdave.mailloux#gmail.com>
2023-09-30 14:36:35 -0400

    changed console.logs to returns

--- block016/block016-workshop.js
+++ block016/block016-workshop.js
@@ -41,15 +41,15 @@ const dave = {
   lname: "Mailloux",
   patientsDoctor: "Dr. Strange",
   prescription: "idarucizumab",
-  pricePerRefill: 75,
+  pricePerRefill: 100,
   refills: 10,
-  refillsRemaining: 2,
-  subscription: subscriptionOptions[0],
-  coupon: couponOptions[0],
+  refillsRemaining: 10,
+  subscription: subscriptionOptions[1],
+  coupon: couponOptions[1],
 };
 
 // Defining Search Value Field for testing purposes.
-let searchName = "dave";
+let searchName = dave;
 
 // Search value pulled from HTML page.
 // document.getElementById('add-name').value = searchName;
@@ -61,7 +61,6 @@ let numRefill = searchName["refills"];
 let numRefillsRemaining = searchName["refillsRemaining"];
 let sub = searchName["subscription"];
 let coup = searchName["coupon"];
-let finalPrice = "";
 
 // Function Determining if Patient is authorized to have their
 // prescription refilled.
@@ -70,14 +69,14 @@ function refillAuthorization() {
     numRefillsRemaining -= 1;
     return numRefillsRemaining && applySubCoupDiscount();
   } else {
-    console.log("no more drugs for you");
+    return `${searchName.fname} ${searchName.lname} is out of refills. Patient should reach out to ${searchName.patientsDoctor} to get their prescription updated.`;
     // alert('${searchName.fname} ${searchName.lname} is out of refills. Patient should reach out to ${patientsDoctor} to get their prescription refill updated.');
   }
 }
 
 // Call on refillAuthorization function and update refillsRemaining variable and
 // it's corresponding object's value.
-searchName["refillsRemaining"] = numRefillsRemaining;
+searchName.refillsRemaining = numRefillsRemaining;
 
 // Function Determining if Patient has a Subscription OR Coupon
 // in order to apply a 25% (subscription) OR $10 (Coupon) discount torwards the final
@@ -85,7 +84,7 @@ searchName["refillsRemaining"] = numRefillsRemaining;
 function applySubCoupDiscount() {
   if (sub === true && coup === true) {
     const finalPrice = price * (1 - subDiscountValue) + coupDiscountValue;
-    return finalPrice;
+    return finalPrice && finalResult(finalPrice);
   } else {
     return applySubDiscount();
   }
@@ -93,7 +92,7 @@ function applySubCoupDiscount() {
 function applySubDiscount() {
   if (sub === true && coup === false) {
     const finalPrice = price * (1 - subDiscountValue);
-    return finalPrice;
+    return finalPrice && finalResult(finalPrice);
   } else {
     return applyCoupDiscount();
   }
@@ -101,18 +100,18 @@ function applySubDiscount() {
 function applyCoupDiscount() {
   if (sub === false && coup === true) {
     const finalPrice = price + coupDiscountValue;
-    return finalPrice;
+    return finalPrice && finalResult(finalPrice);
   } else {
     const finalPrice = price;
-    return finalPrice && finalResult();
+    return finalPrice && finalResult(finalPrice);
   }
 }
 
 // Final function that can be called on to fun the whole program.
-function finalResult() {
-  return `${searchName.fname} ${searchName.lname} has ${searchName.refillsRemaining} refills remaining. Their grand total is $${refillAuthorization()}`;
+function finalResult(finalPrice) {
+  return `${searchName.fname} ${searchName.lname} has ${searchName.refillsRemaining} refills remaining. Their grand total is $${finalPrice}`;
 }
 
 // console.log(finalResult());
 // console.log(searchName);
-console.log(finalResult());
+console.log(refillAuthorization());
