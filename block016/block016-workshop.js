37989b61d9cf6904ded3d510c220cfc2cdced2ee
Hoplite2A <stgdave.mailloux#gmail.com>
2023-09-30 13:57:57 -0400

    added additional funciton to generate all code together. Need to work on making html field input work and button trigger final funciton.

--- block016/block016-workshop.js
+++ block016/block016-workshop.js
@@ -4,6 +4,9 @@ const subDiscountValue = 0.25;
 const coupDiscountValue = -10;
 
 const timmy = {
+    fname: 'Timmy',
+    lname: 'Truner',
+    patientsDoctor: 'Dr. Pepper',
     prescription: 'acetaminophen',
     pricePerRefill: 25,
     refills: 3,
@@ -12,6 +15,9 @@ const timmy = {
     coupon: couponOptions[1],
 };
 const sarah = {
+    fname: 'Sarah',
+    lname: 'Silverstein',
+    patientsDoctor: 'Doc. Ock',
     prescription: 'diphenhydramine',
     pricePerRefill: 50,
     refills: 1,
@@ -20,6 +26,9 @@ const sarah = {
     coupon: couponOptions[0],
 };
 const rocky = {
+    fname: 'Rocky',
+    lname: 'Balboa',
+    patientsDoctor: 'Dr. Jane Foster',
     prescription: 'phenylephrine',
     pricePerRefill: 30,
     refills: 5,
@@ -27,37 +36,55 @@ const rocky = {
     subscription: subscriptionOptions[1],
     coupon: couponOptions[1],
 };
+const dave = {
+    fname: 'Dave',
+    lname: 'Mailloux',
+    patientsDoctor: 'Dr. Strange',
+    prescription: 'idarucizumab',
+    pricePerRefill: 75,
+    refills: 10,
+    refillsRemaining: 2,
+    subscription: subscriptionOptions[0],
+    coupon: couponOptions[0],
+};
 
 // Defining Search Value Field
-let searchName = document.getElementById('add-name').value;
+let searchName = dave;
+    // document.getElementById('add-name').value = searchName;
 // let searchName = sarah;
 
 // Defining new changable vairables for ease of use in future functions
 let prescription = searchName['prescription'];
 let price = searchName['pricePerRefill'];
-let numRefilled = searchName['refills'];
+let numRefill = searchName['refills'];
 let numRefillsRemaining = searchName['refillsRemaining'];
 let sub = searchName['subscription'];
 let coup = searchName['coupon'];
+let finalPrice = "";
 
 // Function Determining if Patient is authorized to have their
 // prescription refilled.
+
 function refillAuthorization() {
     if (numRefillsRemaining >= 1) {
         numRefillsRemaining -= 1;
         return numRefillsRemaining && applySubCoupDiscount();
     } else {
-        console.log('Patient is out of refills. Patient should reach out to Doctor to set up another appointment.');
+        alert('${searchName.fname} ${searchName.lname} is out of refills. Patient should reach out to ${patientsDoctor} to get their prescription refill updated.');
     }    
 };
 
-// Call on refillAuthorization function and update refillsRemaining variable and 
-// it's corresponding object's value.
 searchName['refillsRemaining'] = numRefillsRemaining;
+
+// Call on refillAuthorization function and update refillsRemaining variable and
+// it's corresponding object's value.
 
 // Function Determining if Patient has a Subscription OR Coupon
 // in order to apply a 25% (subscription) OR $10 (Coupon) discount torwards the final
 // price of their prescription.
+
+
+//  WORKING WITHOUT ISSUE!!!!!
 function applySubCoupDiscount() {
     if (sub === true && coup === true) {
         const finalPrice = (price * (1 - subDiscountValue)) + coupDiscountValue;
@@ -84,11 +111,11 @@ function applyCoupDiscount() {
     }
 };
 
-console.log(refillAuthorization());
-
-function grandTotal() {
-    refillAuthorization();
-    return console.log(`${searchName} your grand total is $${finalPrice}.`);
+function finalResult() {
+    console.log(
+                `${searchName.fname} ${searchName.lname} has ${searchName.refillsRemaining} refills remaining. Their grand total is $${refillAuthorization()}`
+                );
 };
 
-grandTotal();
+console.log(finalResult());
+console.log(searchName);
